fa58a2e0f47d946f404922fbec6782dc0381c22b
Facundo <facundoarueda@hotmail.com>
2023-02-05 19:53:30 -0300

    feat: DEV clean code

--- react-tailwind-01/src/components/InputDolar.jsx
+++ react-tailwind-01/src/components/InputDolar.jsx
@@ -3,42 +3,24 @@ import { MyContext } from '../context/ContextProvider';
 import { DataContext } from '../context/DataContext';
 import { useParams } from 'react-router-dom';
 import Country from './Country';
+import data from '../statics/data/countries.json';
 
 const InputDolar = () => {
   const { setState } = useContext(MyContext);
   var dolarOficial = useContext(DataContext);
   let { country } = useParams();
-
+  const countries = data.countries;
 
   const handleValueChange = e => {
     e.preventDefault();
     setState(e.target.value * dolarOficial);
   }
-
-  const countries = [
-    {
-      country: 'Mexico',
-      description: 'Cantidad de Pesos Mexicanos (MX)'
-    },
-    {
-      country: 'EEUU',
-      description: 'Cantidad de dolares (USD)'
-    },
-    {
-      country: 'Europa',
-      description: 'Cantidad de Euros (€)'
-    },
-    {
-      country: 'Brasil',
-      description: 'Cantidad de Reales (R)'
-    },
-  ]
-
+  
   const result = countries.filter(item => item.country === country);
 
   return (
     <div>
-      <label htmlFor="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{result[0].description}</label>
+      <label htmlFor="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{result[0].title}</label>
       <div className="flex">
         <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
           <Country country={result[0].country} />
