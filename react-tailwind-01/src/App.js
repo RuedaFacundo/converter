37fcfa13543172bd68c08b52b0bc02240395fe04
Facundo <facundoarueda@hotmail.com>
2023-02-11 17:46:12 -0300

    feat: DEV add value exchange rates

--- react-tailwind-01/src/App.js
+++ react-tailwind-01/src/App.js
@@ -1,37 +1,30 @@
 import './App.css';
 import Home from './container/Home';
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
 import { ContextProvider } from './context/ContextProvider';
 import { DataContext } from './context/DataContext';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Error404 from './container/Error404';
 import Converter from './container/Converter';
 import Header from './container/Header';
+import GetCurrencyCountries from './utils/dolarValue';
 
 function App() {
-  const [data, setData] = useState([]);
 
-  var config = {
-    method: 'get',
-    url: 'https://cors-anywhere.herokuapp.com/https://api.estadisticasbcra.com/usd_of',
-    headers: { 
-      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDcwNzQ2MzEsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmYWN1bmRvcnVlZGExNEBnbWFpbC5jb20ifQ.owAtmU5VNLcor7RbMWS_Yg9-Rv1AkXLowch7-_S6SOmX4yiYQP6pfJeB1sttSS7gIDmuKTHdmSLv38Jz1TXn2Q'
+  const [cotizaciones, setCotizaciones] = useState(null);
+  const flagRef = useRef(true);
+
+  useEffect(() => {
+    if(flagRef.current === true) {
+      GetCurrencyCountries().then(data => {
+      setCotizaciones(data);
+      flagRef.current = false;
+      });
     }
-  };
-  
-  axios(config)
-  .then(function (response) {
-    var json = response.data;
-    const lastObject = json.slice(-1)[0];
-    setData(lastObject.v);
-  })
-  .catch(function (error) {
-    console.log('Error al obtener el valor del dolar actual');
-  });
+  }, []);
 
   return (
-    <DataContext.Provider value={data}>
+    <DataContext.Provider value={cotizaciones}>
       <ContextProvider>
         <BrowserRouter>
           <Header />
